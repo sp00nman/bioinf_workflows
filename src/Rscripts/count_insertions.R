67755bf77a8bb94e3e4ec740baf909d0b81cf957
Fiorella Schischlik <fiorella.schischlik@gmail.com>
2015-03-15 23:39:48 +0100

    count mutagenic insertions

--- src/Rscripts/count_insertions.R
+++ src/Rscripts/count_insertions.R
@@ -21,16 +21,29 @@ separate.silent.mutagenic.insertions <- function(df_insertions){
   return(list(mutagenic, silent))
 }
 
-count.insertions <- function(count_exon, count_intron){
-  # merge
-  # count only unique insertions
-  # 
+count.mutagenic.insertions <- function(count_exon, count_intron){
+  mutagenic_insertions <- rbind(count_exon, count_intron)
+  genes <- unique(mutagenic_insertions$genesymbol)
+  mutagenic_insertions$genesymbol <- factor(mutagenic_insertions$genesymbol, levels=genes)
+  count_table <- as.data.frame(table(mutagenic_insertions$genesymbol))
+  # only count unique insertions
+  total_insertions <- length(unique(mutagenic_insertions$id))
+  count_table$no_insertions <- total_insertions - count_table$Freq
+  return(count_table)
 }
 
+write2file <- function(count_table, filename){
+  fn_out <- filename
+  write.table(count_table, file=paste(path,fn_out,sep=""),
+              append=F, quote=F, 
+              row.names=T, col.names=T, 
+              dec=".", sep="\t")
+}
 
 options <- commandArgs(trailingOnly=TRUE)
 print options
 #read.count.tables(options[1], options[2])
+file_out = "outfile_count_table"
 exon_f = "../../test/TEST.insertions.exon.bed"
 intron_f = "../../test/TEST.insertions.intron.bed"
 
@@ -38,4 +51,7 @@ intron_insertions <- read.count.tables(intron_f)
 exon_insertions <- read.count.tables(exon_f)
 
 orientation <- separate.silent.mutagenic.insertions(intron_insertions)
+mutagenic_intron <- orientation[[1]]
+count_table <- count.mutagenic.insertions(exon_insertions, mutagenic_intron)
+write2file(count.table, filename = file_out )
 
